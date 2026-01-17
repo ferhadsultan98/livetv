3caae8f37e99bceb9a8a45675347ee8738e1c2f1
Farhad Sultanov <sultanovv991@gmail.com>
2026-01-17 18:02:36 +0400

    Create channels.js

---
+++ channels.js
@@ -0,0 +1,103 @@
+// src/channels.js
+
+const channels = [
+  {
+    id: 1,
+    name: "Alvin Channel TV",
+    logo: "https://via.placeholder.com/50", // Logo şəkli tapanda bura qoyarsan
+    url: "https://cdn10-alvinchannel.yayin.com.tr/alvinchannel/alvinchannel/playlist.m3u8"
+  },
+  {
+    id: 2,
+    name: "AnewZ",
+    url: "https://53be5ef2d13aa.streamlock.net/cubesanewz-secure/smil:cubesanewz-secure-web.smil/playlist.m3u8"
+  },
+  {
+    id: 3,
+    name: "APA TV",
+    url: "http://rtmp.apa.tv/@pagroup!23.m3u8"
+  },
+  {
+    id: 4,
+    name: "ARB",
+    url: "https://raw.githubusercontent.com/UzunMuhalefet/streams/main/myvideo-az/arb.m3u8"
+  },
+  {
+    id: 5,
+    name: "ARB 24",
+    url: "https://raw.githubusercontent.com/UzunMuhalefet/streams/main/myvideo-az/arb-24.m3u8"
+  },
+  {
+    id: 6,
+    name: "ARB Günəş",
+    url: "https://raw.githubusercontent.com/UzunMuhalefet/streams/main/myvideo-az/arb-gunes.m3u8"
+  },
+  {
+    id: 7,
+    name: "Azad TV",
+    url: "https://stream.atv.az/WebRTCAppEE/streams/780339739845112514894920_adaptive.m3u8"
+  },
+  {
+    id: 8,
+    name: "Baku TV",
+    url: "https://raw.githubusercontent.com/UzunMuhalefet/streams/refs/heads/main/myvideo-az/baku-tv.m3u8"
+  },
+  {
+    id: 9,
+    name: "CBC Sport",
+    url: "https://mn-nl.mncdn.com/cbcsports_live/cbcsports/playlist.m3u8"
+  },
+  {
+    id: 10,
+    name: "Dünya TV",
+    url: "https://raw.githubusercontent.com/UzunMuhalefet/streams/refs/heads/main/myvideo-az/dunya-tv.m3u8"
+  },
+  {
+    id: 11,
+    name: "EL TV",
+    url: "http://85.132.53.162:1935/live/eltv/playlist.m3u8"
+  },
+  {
+    id: 12,
+    name: "İctimai TV (ITV)",
+    url: "https://live.itv.az/itv.m3u8"
+  },
+  {
+    id: 13,
+    name: "Kanal S",
+    url: "https://raw.githubusercontent.com/UzunMuhalefet/streams/refs/heads/main/myvideo-az/kanal-s.m3u8"
+  },
+  {
+    id: 14,
+    name: "Mədəniyyət TV",
+    url: "https://str.yodacdn.net/medeniyyet/index.m3u8"
+  },
+  {
+    id: 15,
+    name: "MTV Azərbaycan",
+    url: "https://raw.githubusercontent.com/UzunMuhalefet/streams/refs/heads/main/myvideo-az/mtv-azerbaycan.m3u8"
+  },
+  {
+    id: 16,
+    name: "Space TV",
+    url: "https://raw.githubusercontent.com/UzunMuhalefet/streams/main/myvideo-az/space-tv.m3u8"
+  },
+  {
+    id: 17,
+    name: "Xəzər TV",
+    url: "https://raw.githubusercontent.com/UzunMuhalefet/streams/main/myvideo-az/xezer-tv.m3u8"
+  },
+  {
+    id: 18,
+    name: "AzTV",
+    // QEYD: Bu linkdə token var, vaxtı bitəndə yeniləmək lazım ola bilər
+    url: "http://109.205.166.68/server124/aztv/index3.m3u8?st=AzGSZjk1DpXY1SLc1m4OvA&e=1676581573"
+  },
+  {
+    id: 19,
+    name: "Kəpəz TV",
+    url: "http://85.132.78.122:8050/hls/stream/index.m3u8"
+  }
+];
+
+export default channels;
